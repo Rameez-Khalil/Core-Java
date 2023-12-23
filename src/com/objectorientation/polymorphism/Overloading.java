94875d7f45f079c796fdc28b6ca5ff3c87cb1c4e
rameez <rameezkhalil23@gmail.com>
2023-12-23 05:20:06 -0800

    polymorphism1

---
+++ src/com/objectorientation/polymorphism/Overloading.java
@@ -0,0 +1,12 @@
+package com.objectorientation.polymorphism;
+
+public class Overloading {
+
+    public void method1(){
+        System.out.println("Rameez" + 20);
+    }
+
+    public void method1(String name, int id){
+        System.out.println(name + " " + id);
+    }
+}
---
+++ src/com/objectorientation/polymorphism/PolymorphismRunner.java
@@ -0,0 +1,11 @@
+package com.objectorientation.polymorphism;
+
+public class PolymorphismRunner {
+    public static void main(String[] args) {
+        Overloading overloading = new Overloading();
+        overloading.method1();
+        overloading.method1("Rameez1", 30);
+
+        
+    }
+}
