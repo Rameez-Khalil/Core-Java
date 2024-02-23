1af376c70707844e1ec38f5e72d3d681f38eea3f
rameez <rameezkhalil23@gmail.com>
2024-02-23 03:20:19 -0800

    regular expression

---
+++ src/com/bitwise/coding/Main.java
@@ -0,0 +1,13 @@
+package com.bitwise.coding;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        //AND OPERATION
+        int x = 10;
+        int y = 6;
+        System.out.println(x&y);
+
+    }
+}
