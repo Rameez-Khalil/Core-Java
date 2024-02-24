19ec6af9b7b10df325d37f6042e917f2e8b63720
rameez <rameezkhalil23@gmail.com>
2024-02-24 08:19:11 -0800

    string, regular expressions, challenges

--- src/com/String/code/StringChallenge.java
+++ src/com/String/code/StringChallenge.java
@@ -1,5 +1,7 @@
 package com.String.code;
 
+import java.util.Arrays;
+
 public class StringChallenge {
     public static void main(String[] args) {
 
@@ -12,6 +14,39 @@ public class StringChallenge {
         String domainName = str.substring(str.indexOf("@")+1,str.indexOf("."));
         System.out.println("Username : " + username);
         System.out.println("Domain Name : " + domainName);
+
+
+//        Find if the number is binary.
+        int binary = 10110001;
+        String stringRepresentation = String.valueOf(binary);
+        System.out.println("BINARY NUMBER");
+        System.out.println(stringRepresentation.matches("[01]*"));
+
+
+//        Find if the number is hexadecimal.
+        String hexNumber = "234A";
+        System.out.println(hexNumber.matches("[0-9A-F]+"));
+
+
+//        Write regular expressions for date and time.
+        String dateTime = "01/01/200";
+        System.out.println(dateTime.matches("[0-1][0-9]/[01][0-9]/[0-9]{4}"));
+
+
+//        Remove special characters.
+        String specialCharacters = "abcd@!";
+        String newString = specialCharacters.replaceAll("[^a-zA-Z0-9]", "");
+        System.out.println(newString);
+
+//        Remove extra spaces.
+        String extraSpace = "acd  ";
+        System.out.println(extraSpace.replaceAll("\\s", ""));
+
+//        Find number of words.
+        String numberOfWords = "rameez is the greatest";
+        String[] strArray = numberOfWords.split("\\s");
+        System.out.println(strArray.length);
+
 
     }
 }
